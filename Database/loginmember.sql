011a70b242b6ac6bc2fd725f53aefbf9a039f344
NguyenTaTrung <45004246+NguyenTaTrung@users.noreply.github.com>
2018-12-22 21:42:17 +0700

    database

--- Database/loginmember.sql
+++ Database/loginmember.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `users` (
-  `id_user` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL PRIMARY KEY,
   `username` text NOT NULL,
   `password` text NOT NULL,
   `date_created` datetime NOT NULL
