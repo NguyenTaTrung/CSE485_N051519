ca76c6979cdf184375564da1605d3c3b454d169b
NguyenTaTrung <45004246+NguyenTaTrung@users.noreply.github.com>
2018-12-29 20:37:31 +0700

    Database

--- Database/loginmember.sql
+++ Database/loginmember.sql
@@ -1,8 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th12 29, 2018 lúc 09:02 AM
+-- Phiên bản máy phục vụ: 10.1.36-MariaDB
+-- Phiên bản PHP: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `loginmember`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `congty`
+--
+
+CREATE TABLE `congty` (
+  `MaCT` int(11) NOT NULL,
+  `TenCT` varchar(30) NOT NULL,
+  `MaKV` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khuvuc`
+--
+
+CREATE TABLE `khuvuc` (
+  `MaKV` int(11) NOT NULL,
+  `TenKV` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nganhnghe`
+--
+
+CREATE TABLE `nganhnghe` (
+  `MaNN` int(11) NOT NULL,
+  `TenNN` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `thongtin`
+--
+
+CREATE TABLE `thongtin` (
+  `MaTT` int(11) NOT NULL,
+  `Logo` varchar(20) NOT NULL,
+  `SoLuongTuyen` int(11) NOT NULL,
+  `YeuCau` varchar(200) NOT NULL,
+  `Luong` varchar(15) NOT NULL,
+  `NgayDang` datetime NOT NULL,
+  `MaVL` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
 CREATE TABLE `users` (
-  `id_user` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
   `firsName` varchar(15) NOT NULL,
   `lastName` varchar(15) NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL,
   `date_created` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`id_user`, `firsName`, `lastName`, `username`, `password`, `date_created`) VALUES
+(1, 'trung', 'nguyen', 'trung', '123456tT', '2018-12-29 14:57:26');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `vieclam`
+--
+
+CREATE TABLE `vieclam` (
+  `MaVL` int(11) NOT NULL,
+  `TenVL` varchar(50) NOT NULL,
+  `MaCT` int(11) NOT NULL,
+  `MaNN` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `congty`
+--
+ALTER TABLE `congty`
+  ADD PRIMARY KEY (`MaCT`),
+  ADD KEY `MaKV` (`MaKV`);
+
+--
+-- Chỉ mục cho bảng `khuvuc`
+--
+ALTER TABLE `khuvuc`
+  ADD PRIMARY KEY (`MaKV`);
+
+--
+-- Chỉ mục cho bảng `nganhnghe`
+--
+ALTER TABLE `nganhnghe`
+  ADD PRIMARY KEY (`MaNN`);
+
+--
+-- Chỉ mục cho bảng `thongtin`
+--
+ALTER TABLE `thongtin`
+  ADD PRIMARY KEY (`MaTT`),
+  ADD KEY `MaVL` (`MaVL`);
+
+--
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Chỉ mục cho bảng `vieclam`
+--
+ALTER TABLE `vieclam`
+  ADD PRIMARY KEY (`MaVL`),
+  ADD KEY `MaCT` (`MaCT`),
+  ADD KEY `MaNN` (`MaNN`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `congty`
+--
+ALTER TABLE `congty`
+  MODIFY `MaCT` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `khuvuc`
+--
+ALTER TABLE `khuvuc`
+  MODIFY `MaKV` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `nganhnghe`
+--
+ALTER TABLE `nganhnghe`
+  MODIFY `MaNN` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `thongtin`
+--
+ALTER TABLE `thongtin`
+  MODIFY `MaTT` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `vieclam`
+--
+ALTER TABLE `vieclam`
+  MODIFY `MaVL` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `congty`
+--
+ALTER TABLE `congty`
+  ADD CONSTRAINT `congty_ibfk_1` FOREIGN KEY (`MaKV`) REFERENCES `khuvuc` (`MaKV`);
+
+--
+-- Các ràng buộc cho bảng `thongtin`
+--
+ALTER TABLE `thongtin`
+  ADD CONSTRAINT `thongtin_ibfk_1` FOREIGN KEY (`MaVL`) REFERENCES `vieclam` (`MaVL`);
+
+--
+-- Các ràng buộc cho bảng `vieclam`
+--
+ALTER TABLE `vieclam`
+  ADD CONSTRAINT `vieclam_ibfk_1` FOREIGN KEY (`MaCT`) REFERENCES `congty` (`MaCT`),
+  ADD CONSTRAINT `vieclam_ibfk_2` FOREIGN KEY (`MaNN`) REFERENCES `nganhnghe` (`MaNN`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
